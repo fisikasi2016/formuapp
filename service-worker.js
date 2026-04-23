da43ed08f2fdc24c0172c9e6ed83e1f3941c4aa2
fisikasi <ibonrec@gmail.com>
2026-04-23 18:34:28 +0200

    instalazio botoia

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = "aditzak-pwa-v1";
+const CACHE_NAME = "formuapp-pwa-v8";
 
 const archivosAguardar = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./service-worker.js"
+  "./service-worker.js",
+  "./icono-192.png",
+  "./icono-512.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -13,12 +15,50 @@ self.addEventListener("install", (event) => {
       return cache.addAll(archivosAguardar);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then((respuesta) => {
-      return respuesta || fetch(event.request);
+    caches.match(event.request).then((respuestaCache) => {
+      if (respuestaCache) {
+        return respuestaCache;
+      }
+
+      return fetch(event.request)
+        .then((respuestaRed) => {
+          if (!respuestaRed || respuestaRed.status !== 200 || respuestaRed.type !== "basic") {
+            return respuestaRed;
+          }
+
+          const copiaRespuesta = respuestaRed.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copiaRespuesta);
+          });
+
+          return respuestaRed;
+        })
+        .catch(() => {
+          return caches.match("./index.html");
+        });
     })
   );
 });
